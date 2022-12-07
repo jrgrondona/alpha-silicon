869709a62a0bcaf6498995ed96835395bdf8858c
JUAN RAMON GRONDONA <113069231+jrgrondona@users.noreply.github.com>
2022-12-07 10:41:58 -0300

    Update Dump20221129.sql

--- backupdb/Dump20221129.sql
+++ backupdb/Dump20221129.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: alpha-silicon-01
+-- Host: localhost    Database: alpha-silicon
 -- ------------------------------------------------------
--- Server version	8.0.31-0ubuntu0.20.04.1
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,61 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
+  `idcurso` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` text,
+  `estado` varchar(1) DEFAULT '1',
+  PRIMARY KEY (`idcurso`),
+  UNIQUE KEY `idcurso_UNIQUE` (`idcurso`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,'DFS','developer full stack','1'),(2,'DS','data science','1'),(3,'IP','introduccion a la programacion','1'),(4,'MYSQL','base de datos','1'),(5,'IDIOMA','ingles beginner','1');
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materias`
+--
+
+DROP TABLE IF EXISTS `materias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materias` (
+  `idmateria` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `objetivo` text,
+  `plan_estudio` text,
+  `estado` varchar(1) DEFAULT '1',
+  PRIMARY KEY (`idmateria`),
+  UNIQUE KEY `idmaterias_UNIQUE` (`idmateria`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materias`
+--
+
+LOCK TABLES `materias` WRITE;
+/*!40000 ALTER TABLE `materias` DISABLE KEYS */;
+INSERT INTO `materias` VALUES (1,'ingles','basico','6 meses','1'),(2,'lenguaje de programacion','desarrollo web','6 meses','1'),(3,'matematica','calculos','3 meses','1'),(4,'filosofia','pensamiento logico','5 meses','1'),(5,'github','avances de proyectos','6 meses','0');
+/*!40000 ALTER TABLE `materias` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `personas`
@@ -45,6 +100,67 @@ LOCK TABLES `personas` WRITE;
 INSERT INTO `personas` VALUES (1,'Antonia','Lopez','42133221','F','2001-11-02','1'),(2,'Gabriela','Gutierres','32337879','F','1998-02-03','1'),(3,'Juan Gabriel','Rosas','32245566','M','1988-02-03','1'),(4,'Roberto','Carlos','31998876','M','1986-02-03','1'),(5,'Julian','Alcaraz','29654876','M','1978-12-05','1'),(6,'Florecia','Gimenez','43454612','F','2002-07-12','1'),(7,'Juana','Richmond','46454612','F','2007-07-12','1'),(8,'Ramona','Gimenez','41287346','F','2001-11-27','1'),(9,'Andres','Kuhn','31454612','M','1985-07-12','1');
 /*!40000 ALTER TABLE `personas` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `sedes`
+--
+
+DROP TABLE IF EXISTS `sedes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sedes` (
+  `idsede` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `direccion` text,
+  `localidad` varchar(50) DEFAULT NULL,
+  `cod_postal` varchar(10) DEFAULT NULL,
+  `telcontacto1` varchar(15) DEFAULT NULL,
+  `telcontacto2` varchar(15) DEFAULT NULL,
+  `estado` varchar(1) DEFAULT '1',
+  PRIMARY KEY (`idsede`),
+  UNIQUE KEY `idsedes_UNIQUE` (`idsede`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sedes`
+--
+
+LOCK TABLES `sedes` WRITE;
+/*!40000 ALTER TABLE `sedes` DISABLE KEYS */;
+INSERT INTO `sedes` VALUES (1,'cic','av san martin','eldorado','3380','3751455566','3764564565','1'),(2,'kolping','av 9 de julio','puerto rico','3334','3743456978','3743552331','1'),(3,'polotic','av cuaranta','posadas','3330','3764000245','3765054658','1'),(4,'cedemu','av andres guacurari 150','garuhape','3334','3743336585','3757987855','1'),(5,'oxford','calle sin numero','capiovi','3334','3743789845','3743667744','0');
+/*!40000 ALTER TABLE `sedes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idusuario` int NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `estado` varchar(1) DEFAULT '1',
+  PRIMARY KEY (`idusuario`),
+  UNIQUE KEY `idusuarios_UNIQUE` (`idusuario`),
+  UNIQUE KEY `nickname_UNIQUE` (`nickname`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'jrgrondona','juangrondona.01@gmail.com','prueba','1'),(2,'ggomez','ggomez@hotmail.com','test','1'),(3,'jalcaraz','julian@yahoo.com','nuevo','1'),(4,'rgimenez','ramona@noposee.com','lobo','1'),(5,'ggutierres','gabriela@live.com','luna','0');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -55,4 +171,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-29 18:04:33
+-- Dump completed on 2022-12-07 10:34:24
